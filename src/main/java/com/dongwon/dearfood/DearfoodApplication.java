c4946e4d88a03330e268fa28578d03de6b11fe2e
임또이 <sh663746@gmail.com>
2021-07-26 13:13:26 +0900

    init commit

---
+++ src/main/java/com/dongwon/dearfood/DearfoodApplication.java
@@ -0,0 +1,13 @@
+package com.dongwon.dearfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DearfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DearfoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dongwon/dearfood/DearfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongwon.dearfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DearfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
