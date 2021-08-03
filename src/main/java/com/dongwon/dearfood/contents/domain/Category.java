5864922d33fdc59350e26b8c3946f4022868120c
임또이 <sh663746@gmail.com>
2021-08-03 17:22:46 +0900

    category 조회 성공

--- src/main/java/com/dongwon/dearfood/contents/domain/Category.java
+++ src/main/java/com/dongwon/dearfood/contents/domain/Category.java
@@ -1,4 +1,15 @@
 package com.dongwon.dearfood.contents.domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class Category {
+    private String level1;
+    private String level2;
+    private String level3;
+    private String level4;
 }
