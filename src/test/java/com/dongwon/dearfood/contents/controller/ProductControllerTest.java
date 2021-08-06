b2124dfd96590fa67429b9723317275f498e2503
임또이 <sh663746@gmail.com>
2021-08-06 10:42:46 +0900

    shouldUpdatePrice 테스트 성공

--- src/test/java/com/dongwon/dearfood/contents/controller/ProductControllerTest.java
+++ src/test/java/com/dongwon/dearfood/contents/controller/ProductControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 import org.mockito.InjectMocks;
@@ -16,6 +17,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
@@ -39,12 +41,17 @@ public class ProductControllerTest {
     @Before
     public void setUp() {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(wac)
-                .addFilters(new CharacterEncodingFilter("UTF-8",true))
+                .addFilters(new CharacterEncodingFilter("UTF-8", true))
                 .build();
     }
 
+    @Test
+    public void shouldFindProductById() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get(URI + "subcategory?keyword=8")
+                .accept(MediaType.APPLICATION_JSON)).andDo(print())
+                .andExpect(jsonPath("$", hasSize(1))).andReturn();
 
-
+    }
 
     @Test
     @DisplayName("유효하지 않은 카테고리 아이디")
@@ -55,8 +62,38 @@ public class ProductControllerTest {
                 .andDo(print())
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.errorMessage")
-                .value("존재하지 않는 아이디 입니다."))
+                        .value("존재하지 않는 아이디 입니다."))
+                .andExpect((jsonPath("$.errorCode")
+                        .value("NO_EXIST_ID")))
                 .andReturn();
 
     }
+
+    @Test
+    public void shouldUpdatePrice() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.patch(URI + "/1/price?modifyPrice=20000")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.productId")
+                        .value(1))
+                .andExpect(jsonPath("$.status")
+                        .value("[상품번호 :1] 의 가격이 업데이트 되었습니다.")).andReturn();
+
+    }
+
+    @Test
+    public void shouldVerifyInvalidUpdate() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.patch(URI + "/100/price?modifyPrice=20000")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errorCode")
+                        .value("NO_EXIST_ID"))
+                .andExpect(jsonPath("$.errorMessage")
+                        .value("존재하지 않는 아이디 입니다.")).andReturn();
+    }
+
 }
